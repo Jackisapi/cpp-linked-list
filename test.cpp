b0ed1ded8d28babe53f266caf2fcf9d8dc6d7382
Jack W <getphyk3d@gmail.com>
2024-12-17 20:44:13 -0700

    Updated test.cpp

--- test.cpp
+++ test.cpp
@@ -6,31 +6,34 @@
 #include "LinkedList.h"
 
 int main() {
-    // LinkedList list;
-    // for (int i = 1; i <= 10; i++) {
-    //     list.append(i);
-    // }
-    // list.removeNode(1);
-    // list.printList();
-    // list.push();
-    // list.printList();
-    //
-    // for (int i = 1; i <= 10; i++) {
-    //     list.append(i);
-    // }
-    // list.pop();
-    // list.printList();
-    //
-    // for (int i = 1; i <= 10; i++) {
-    //     list.append(i);
-    // }
-    // std::cout << list.index_node(3) << std::endl;
-    // std:: cout << list.len();
-
     LinkedList list;
+    for (int i = 1; i <= 10; i++) {
+        list.append(i);
+    }
+    list.removeNode(1);
+    list.printList();
+    list.push();
+    list.printList();
+
+    for (int i = 1; i <= 10; i++) {
+        list.append(i);
+    }
+    list.pop();
+    list.printList();
+
+    for (int i = 1; i <= 10; i++) {
+        list.append(i);
+    }
+    std::cout << list.index_node(3) << std::endl;
+    std:: cout << list.len() << std::endl;
+    list.pop();
+
+    std::cout << std::endl;
+
     for (int i = 10; i >= 1; i--) {
         list.append(i);
     }
+    list.printList();
     list.sort();
     list.printList();
 
